c19650be9a21d2000c3566615e5cc9df0e22f1f6
Ichizuchi <144344807+Ichizuchi@users.noreply.github.com>
2024-02-19 12:07:48 +0000

    Индивидуальное задание

---
+++ individual tasks/ind_1.py
@@ -0,0 +1,21 @@
+def starts_with_vowel(word):
+    vowels = "aeiouAEIOU"
+    return word[0] in vowels
+
+def main():
+    file_name = input("cat_text.txt")
+
+    try:
+        with open(file_name, 'r') as file:
+            text = file.read()
+            words = text.split()
+
+            print("Words starting with vowels:")
+            for word in words:
+                if starts_with_vowel(word):
+                    print(word)
+    except FileNotFoundError:
+        print("File not found. Please make sure the file exists.")
+
+if __name__ == "__main__":
+    main()
